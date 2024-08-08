4c202df3a3e881f2d993931d3e878a510a06290e
Tolga Kurtuluş <tolgakurtulus95@gmail.com>
2024-08-08 15:41:59 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
 from feedgen.feed import FeedGenerator
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 import pandas as pd
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 # Sample data retrieval function with IATA code filtering
 def get_data_from_source(source, iata_code=None):
@@ -21,11 +29,6 @@ def get_data_from_source(source, iata_code=None):
 
         return filtered_items        
         
-    
-def get_data_from_source2():
-    items = pd.read_excel("./data/rssdf.xlsx")
-    return items
-
 @app.get("/rss/{iata_code}")
 def generate_rss_feed(iata_code: str, source: str = "excel"):
     fg = FeedGenerator()
@@ -47,23 +50,6 @@ def generate_rss_feed(iata_code: str, source: str = "excel"):
     rss_feed = fg.rss_str(pretty=True)
     return Response(content=rss_feed, media_type='application/rss+xml')
 
-@app.get("/rss")
-def generate_rss_feed2(source: str = "excel"):
-    fg = FeedGenerator()
-    fg.title('My RSS Feed')
-    fg.link(href='http://www.example.com', rel='alternate')
-    fg.description('This is an example RSS feed')
-
-    items = get_data_from_source2()
-
-    if items.empty:
-        raise HTTPException(status_code=404, detail="No items found for the given IATA code")
-
-    for index, item in items.iterrows():
-        fe = fg.add_entry()
-        fe.title(str(item['Title']))
-        fe.description(str(item['Body']))
-        fe.link(href=str(item['Link']))
-
-    rss_feed = fg.rss_str(pretty=True)
-    return Response(content=rss_feed, media_type='application/rss+xml')
+@app.get("/", response_class=HTMLResponse)
+async def read_home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
